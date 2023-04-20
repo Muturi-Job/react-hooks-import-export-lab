ab2c8d687be9b75a1f4d08a7297a02cf9cf6b35f
Job Muturi <muirimuturi17@gmail.com>
2023-04-20 10:39:47 +0300

    complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { city, username} from "../data/user";
+
 
 function Home() {
   return (
@@ -9,3 +11,4 @@ function Home() {
     </div>
   );
 }
+export default Home;
